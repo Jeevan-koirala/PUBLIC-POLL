11e2ffad14d28ea485e521e193d6a0aa81ee2c1c
Jeevan-koirala <koiralajeevan019@gmail.com>
2025-08-07 00:21:11 +0545

    Update poll.ts

--- Public poll/pages/api/poll.ts
+++ Public poll/pages/api/poll.ts
@@ -1,9 +1,12 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import { pollDB } from './db';
-export default function handler(req, res) {
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query;
-  if (!pollDB[id]) {
+
+  if (!id || typeof id !== 'string' || !pollDB[id]) {
     return res.status(404).json({ error: 'Poll not found' });
   }
+
   res.status(200).json(pollDB[id]);
 }
-import { pollDB } from './db';
