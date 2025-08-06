bb20b37a365c5df2189898e03183bd00bef9896a
Jeevan-koirala <koiralajeevan019@gmail.com>
2025-08-07 00:15:41 +0545

    Update create.ts

--- Public poll/pages/api/create.ts
+++ Public poll/pages/api/create.ts
@@ -1,15 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import { pollDB } from './db';
 
-export default function handler(req, res) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { question, options } = req.body;
+
     const id = Math.random().toString(36).substring(2, 8);
     pollDB[id] = {
       question,
       options,
       votes: Array(options.length).fill(0),
     };
+
     res.status(200).json({ id });
   } else {
     res.status(405).end();
   }
+}
